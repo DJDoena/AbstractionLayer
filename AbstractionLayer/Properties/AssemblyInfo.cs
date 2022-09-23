3b19046e92e9577076fd6431499c97e9ca3b3238
DJ Doena <github@doena-soft.de>
2022-09-23 19:28:36 +0200

    2.0.1

--- AbstractionLayer/Properties/AssemblyInfo.cs
+++ AbstractionLayer/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Doena Soft.")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright © Doena Soft. 2016 - 2021")]
+[assembly: AssemblyCopyright("Copyright © Doena Soft. 2016 - 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
