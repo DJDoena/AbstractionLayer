188e79f5c4e6d10c576089248db96aa5bbd5ada2
DJDoena <github@doena-soft.de>
2023-08-28 18:20:36 +0200

    increase UI dep to 101

--- AbstractionLayer.WPF/UIServices/WindowClipboardServices.cs
+++ AbstractionLayer.WPF/UIServices/WindowClipboardServices.cs
@@ -34,5 +34,17 @@
                 return false;
             }
         }
+
+        /// <summary>
+        /// Clears the Clipboard and then attempts to place data on it the specified number
+        /// of times and with the specified delay between attempts, optionally leaving the
+        /// data on the Clipboard after the application exits.
+        /// </summary>
+        /// <param name="data">The data to place on the Clipboard.</param>
+        /// <param name="copy">true if you want data to remain on the Clipboard after this application exits; otherwise, false.</param>
+        /// <param name="retryTimes">The number of times to attempt placing the data on the Clipboard.</param>
+        /// <param name="retryDelay">The number of milliseconds to pause between attempts.</param>
+        public void SetDataObject(object data, bool copy, int retryTimes, int retryDelay)
+            => System.Windows.Clipboard.SetDataObject(data, copy);
     }
 }
