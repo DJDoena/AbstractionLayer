c6b9ebcdcd7d00ad6f2c62fede495a766fe55e55
DJDoena <github@doena-soft.de>
2023-08-28 18:11:13 +0200

    AbstractionLayer.UI 1.0.1

--- AbstractionLayer.UI/Contracts/IClipboardServices.cs
+++ AbstractionLayer.UI/Contracts/IClipboardServices.cs
@@ -22,5 +22,16 @@
         /// <param name="text">The text</param>
         /// <returns>If the setting succeeded</returns>
         bool SetText(string text);
+
+        /// <summary>
+        ///    Clears the Clipboard and then attempts to place data on it the specified number
+        ///    of times and with the specified delay between attempts, optionally leaving the
+        ///    data on the Clipboard after the application exits.
+        /// </summary>
+        /// <param name="data">The data to place on the Clipboard.</param>
+        /// <param name="copy">true if you want data to remain on the Clipboard after this application exits; otherwise, false.</param>
+        /// <param name="retryTimes">The number of times to attempt placing the data on the Clipboard.</param>
+        /// <param name="retryDelay">The number of milliseconds to pause between attempts.</param>
+        void SetDataObject(object data, bool copy, int retryTimes, int retryDelay)
     }
 }
