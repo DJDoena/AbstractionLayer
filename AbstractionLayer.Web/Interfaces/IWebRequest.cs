f56c306185890457cd9e41527ebbe9a21ddd24c6
DJDoena <github@doena-soft.de>
2023-08-28 17:00:51 +0200

    AbstractionLayer.Web 1.0.2

---
+++ AbstractionLayer.Web/Interfaces/IWebRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace DoenaSoft.AbstractionLayer.WebServices
+{
+    /// <summary>
+    /// Interface to access WebRequest.
+    /// </summary>
+    public interface IWebRequest
+    {
+        /// <summary>
+        /// Returns a WebResponse.
+        /// </summary>
+        /// <returns>The WebResponse</returns>
+        Task<IWebResponse> GetResponseAsync();
+    }
+}
---
+++ AbstractionLayer.Web/Interfaces/IWebServices.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace DoenaSoft.AbstractionLayer.WebServices
+{
+    /// <summary>
+    /// Interface to access web services.
+    /// </summary>
+    public interface IWebServices
+    {
+        /// <summary>
+        /// Creates a WebRequest.
+        /// </summary>
+        /// <param name="targetUrl">The URL</param>
+        /// <param name="proxy">The proxy server</param>
+        /// <param name="ci">The language in which the data is requested</param>
+        /// <returns>The WebRequest</returns>
+        IWebRequest CreateWebRequest(string targetUrl, System.Net.IWebProxy proxy = null, CultureInfo ci = null);
+
+        /// <summary>
+        /// Creates a WebClient
+        /// </summary>
+        /// <param name="proxy">The proxy server</param>
+        /// <returns>The WebClient</returns>
+        IWebClient CreateWebClient(System.Net.IWebProxy proxy = null);
+    }
+}
